0c499b54bf548e51b125b246f953f9dc2cd2552e
Asif <asifzamanzisan@gmail.com>
2025-05-05 20:16:16 +0600

    Update implementation-using-adj-list.cpp

--- codes/Graph/implementation-using-adj-list.cpp
+++ codes/Graph/implementation-using-adj-list.cpp
@@ -8,8 +8,23 @@ class Graph{
   public:
     unordered_map<int, list<int>> adj;
     void addEdge(int u, int v, bool direction) {
+        // create an edge from u to v
+        adj[u].push_back(v);
+
         // direction = true -> directed graph
         // direction = false -> undirected graph
+        if (direction == false) {
+            adj[v].push_back(u);
+        }
+    }
+    void printAdjList() {
+        for (auto i:adj) {
+            cout << i.first << "->";
+            for (auto j: i.second) {
+                cout << j << ", ";
+            }
+            cout << "\n";
+        }
     }
 
 };
