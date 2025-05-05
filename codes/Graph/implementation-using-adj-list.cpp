b263e2506842b84d05b73caaecc731389635a61f
Asif <asifzamanzisan@gmail.com>
2025-05-05 21:05:38 +0600

    graph definition and printing

--- codes/Graph/implementation-using-adj-list.cpp
+++ codes/Graph/implementation-using-adj-list.cpp
@@ -3,14 +3,18 @@
 #define ll long long
 using namespace std;
 
+// IMPORTANT //
+template <typename T> // to make it usable for any datatype
+// for example: Graph<int>g, Graph<char>g etc..
+
+// Defining Graph Class
 class Graph{
 // unordered map of an int value and a list
   public:
-    unordered_map<int, list<int>> adj;
-    void addEdge(int u, int v, bool direction) {
+    unordered_map<T, list<int>> adj; // replaced int with T 
+    void addEdge(T u, T v, bool direction) { // replaced int with T
         // create an edge from u to v
         adj[u].push_back(v);
-
         // direction = true -> directed graph
         // direction = false -> undirected graph
         if (direction == false) {
@@ -21,17 +25,26 @@ class Graph{
         for (auto i:adj) {
             cout << i.first << "->";
             for (auto j: i.second) {
-                cout << j << ", ";
+                cout << j << " ";
             }
             cout << "\n";
         }
     }
-
 };
 
+// Main Function
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    
+    cout << "Enter number of nodes and edges: ";
+    int n, m; // number of nodes and edges
+    cin >> n >> m;
+    Graph<int>g; // taking a Graph type variable
+    // using <int> because we made a template 
+    // this enables us to use Graph for other data types too!
+    for (int i = 0; i < m; i++) {
+        int u, v; // input of two node point
+        cin >> u >> v;
+        g.addEdge(u, v, false); // addEdge(u, v, direction)
+    }
+    g.printAdjList();
     return 0;
 }
