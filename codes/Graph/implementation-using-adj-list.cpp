fe5114e0b87a2b7c6fee2b409485b4182d686090
Asif <asifzamanzisan@gmail.com>
2025-05-05 20:11:26 +0600

    Create implementation-using-adj-list.cpp

---
+++ codes/Graph/implementation-using-adj-list.cpp
@@ -0,0 +1,22 @@
+// Graph implementation using adjacency List
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+class Graph{
+// unordered map of an int value and a list
+  public:
+    unordered_map<int, list<int>> adj;
+    void addEdge(int u, int v, bool direction) {
+        // direction = true -> directed graph
+        // direction = false -> undirected graph
+    }
+
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    return 0;
+}
