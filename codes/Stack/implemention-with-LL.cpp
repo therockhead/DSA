cebb555dfe072352edac59f67a8aa8f8115bb8c9
Asif <asifzamanzisan@gmail.com>
2025-05-05 19:46:15 +0600

    Create implemention-with-LL.cpp

---
+++ codes/Stack/implemention-with-LL.cpp
@@ -0,0 +1,21 @@
+// Stack Implementation with LinkedList
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+class Node{
+    int data;
+    Node* next;
+
+    public:
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
+    }
+    
+};
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    return 0;
+}
