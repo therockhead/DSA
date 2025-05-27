5521661ebe63d8f49aa19d138a248088db81fd51
Asif <asifzamanzisan@gmail.com>
2025-05-28 01:04:09 +0600

    started LL on gfg

---
+++ codes/Linked-List/gfg/traverse-dll.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node {
+    int data;
+    Node* prev;
+    Node* next;
+    
+    Node(int d) {
+        data = d;
+        prev = nullptr;
+        next = nullptr;
+    }
+};
+int main () {
+    
+
+}
