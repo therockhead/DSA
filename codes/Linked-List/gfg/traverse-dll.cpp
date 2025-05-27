42c3fa05467199d5843dbe301b789225d2a7bb31
Asif <asifzamanzisan@gmail.com>
2025-05-28 01:57:54 +0600

    traversal-dll.cpp added

--- codes/Linked-List/gfg/traverse-dll.cpp
+++ codes/Linked-List/gfg/traverse-dll.cpp
@@ -11,6 +11,36 @@ class Node {
         next = nullptr;
     }
 };
+
+// forward traversal
+// Time Complexity: O(n), Space Complexity O(1)
+void forwardTraversal(Node* head) {
+	Node* curr = head; // initialize at head
+	while(curr != NULL) {
+		cout << curr->data << " "; // access the data at current node
+		curr = curr->next; // move to the next node
+	}
+	cout << endl;
+}
+
+// forward traversal
+// recursion approach
+// Time Complexity: O(N), Space Complexity O(N)
+void forwardTraveralRecursive(Node* head) {
+	if (head == nullptr) return;
+	cout << head->data << " ";
+	forwardTraversalRecursive(head->next);
+}
+
+// backward traversal
+void backwardTraversal(Node* tail) {
+	Node* curr = tail;
+
+	while(curr != nullptr) {
+		cout << curr->data << " ";
+		curr = curr->prev;
+	}
+}
 int main () {
     
 
