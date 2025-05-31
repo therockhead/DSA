de3e7af3967192bddb210e00665e36b5f4e50145
Asif <asifzamanzisan@gmail.com>
2025-05-31 12:50:34 +0600

    added deleteBefore a node func to the deletion-dll.cpp

--- codes/Linked-List/gfg/delete-dll.cpp
+++ codes/Linked-List/gfg/delete-dll.cpp
@@ -115,6 +115,46 @@ Node* deleteAfterNode(Node* head, int key) {
     delete nodeDelete;
     return head;
 }
+// delete before a given node (key)
+Node* deleteBefore(Node* head, int key) {
+    Node* curr = head;
+
+    // find the key
+    while (curr!=nullptr) {
+        if (curr->data == key) {
+            break;
+        }
+        curr = curr->next;
+    }
+
+    // if curr is nullptr or curr-prev is nullptr
+    // that means, we traversed all the nodes but didn't find the key node
+    // so there is no node to be deleted
+    if (curr == nullptr || curr->prev == nullptr) {
+        return head;
+    }
+
+    // Otherwise
+    // Node to be deleted
+    Node* nodeDelete = curr->prev;
+    // the previous of curr means the node before curr
+
+    // update the prev of the curr node
+    // to the prev  of the node to be deleted
+    curr->prev = nodeDelete->prev;
+
+    // if nodeDelete's prev is not nullptr
+    // update its next pointer to the current node
+    if (nodeDelete->prev != nullptr) {
+        nodeDelete->prev->next = curr;
+    } else {
+        // if nodeDelete is the head node
+        head = curr;
+    }
+
+    delete nodeDelete;
+    return head;
+}
 
 int main () {
     int n;
